03481cddb495e14146a2bb0bfa9417cd83cbabfb
DavidHughesJr <hughes014@yahoo.com>
2022-10-05 18:42:42 -0500

    context update to overview

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,7 +1,10 @@
 
-import { Card, Space, Calendar } from 'antd'
+import { Card, Space, Calendar, Typography } from 'antd'
 import React, { useContext } from 'react'
-import BudgetContext from '../context/BudgetContext';
+import { BudgetContext } from '../context/BudgetContext';
+
+
+const { Title, Text } = Typography;
 
 
 const Overview = () => {
@@ -9,44 +12,40 @@ const Overview = () => {
         console.log(value.format('YYYY-MM-DD'), mode);
     };
 
-    const budget = useContext(BudgetContext)
-
-
-    console.log(budget)
+    const { budget } = useContext(BudgetContext)
 
     return (
         <>
-            <h2> Overview </h2>
+            <Title level={2}>Overview</Title>
             <div className='layout'>
                 <Space direction='vertical'>
-                    <Card className='card-medium' title="Card title" hoverable={true}>
-                        <p></p>
+                    <Card className='card-medium' title="All Transaction" hoverable={true}>
+                        <Text> {budget} </Text>
+                    </Card>
+                    <Card className='card-medium' title="Retirement" hoverable={true} >
+                        <p>Card content</p>
                         <p>Card content</p>
                     </Card>
-                    <Card className='card-medium' title="Card title" hoverable={true} >
-                        <p>Card content</p>
-                        <p>Card content</p>
-                    </Card>
-                    <Card className='card-small' title="Card title" hoverable={true}>
+                    <Card className='card-small' title="Investment" hoverable={true}>
                         <p>Card content</p>
                     </Card>
                 </Space>
                 <Space direction='vertical'>
-                    <Card className='card-medium' title="Card title" hoverable={true}>
+                    <Card className='card-medium' title="Net Worth" hoverable={true}>
                         <p>Card content</p>
                         <p>Card content</p>
                     </Card>
-                    <Card className='card-medium' title="Card title" hoverable={true}>
+                    <Card className='card-medium' title="Money Available" hoverable={true}>
                         <p>Card content</p>
                         <p>Card content</p>
                     </Card>
-                    <Card className='card-small' title="Card title" hoverable={true}>
+                    <Card className='card-small' title="Business" hoverable={true}>
                         <p>Card content</p>
                     </Card>
                 </Space>
                 <Space direction='vertical'>
                     <Calendar className='card-calendar' fullscreen={false} onPanelChange={onPanelChange} />
-                    <Card className='card-large' title="Card title" hoverable={true}>
+                    <Card className='card-large' title="Budget" hoverable={true}>
                         <p>Card content</p>
                         <p>Card content</p>
                     </Card>
--- src/context/BudgetContext.js
+++ src/context/BudgetContext.js
@@ -1,14 +1,33 @@
-import { createContext, useState } from 'react'
+import { createContext, useReducer } from 'react'
+
+
+const BudgetReducer = (state, action) => {
+    switch(action.type){
+        default: return state 
+    }
+}
+
+const initialState = {
+    budget: 10000,
+    expenses: [
+        { id: 1, name: 'netflix', cost: 14.99 },
+        { id: 2, name: 'water', cost: 65.21 }
+    ]
+}
+
 
 export const BudgetContext = createContext()
 
 const BudgetContextProvider = (props) => {
 
-
-    const [budget, setBudget] = useState({money: 22})
+    const [state, dispatch] = useReducer(BudgetReducer, initialState)
     
     return (
-        <BudgetContext.Provider value={budget}>{props.children}</BudgetContext.Provider>
+        <BudgetContext.Provider value={{
+            budget: state.budget,
+            expenses: state.expenses,
+            dispatch
+        }}>{props.children}</BudgetContext.Provider>
     )
 }
 
