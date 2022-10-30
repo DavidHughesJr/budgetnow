bf12baee8769af3817524ebb9258dc227dc66b3d
DavidHughesJr <hughes014@yahoo.com>
2022-10-29 22:59:24 -0500

    budget progress bar

--- src/components/pages/Budget.jsx
+++ src/components/pages/Budget.jsx
@@ -1,8 +1,9 @@
-import { Card, Space, Typography } from 'antd'
+import { Card, Space, Typography, Progress } from 'antd'
 import React, { useContext } from 'react'
 import { BudgetContext } from '../../context/BudgetContextProvider'
+import { InvestmentContext } from '../../context/InvestmentContextProvider';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -14,59 +15,37 @@ const Budget = () => {
   const { Text } = Typography
 
   const { budget } = useContext(BudgetContext)
+  const { categories } = useContext(BudgetContext)
 
   const totalIncome = budget[0].amount + budget[1].amount
 
-  const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
 
-  console.log(budget)
 
   return (
     <>
       <Space direction='horizontal' align=''>
         <Card hoverable='true' style={{ width: '200px' }}>
-        <div>
-          <Text> Your Total Income </Text>
-          <Title style={{ margin: 10 }}>  ${totalIncome} </Title>
-          <div style={{ width: '150px', flexDirection: 'column' }}>
-            <div className='flex-between' >
-              <Text> Primary:</Text>
-              <Text> ${budget[0].amount}</Text>
-            </div>
-            <div className='flex-between' >
-              <Text> Secondary:</Text>
-              <Text> ${budget[1].amount}</Text>
+          <div>
+            <Text> Your Total Income </Text>
+            <Title style={{ margin: 21.5 }}>  ${totalIncome} </Title>
+            <div style={{ width: '150px', flexDirection: 'column' }}>
+              <div className='flex-between' >
+                <Text> Primary:</Text>
+                <Text> ${budget[0].amount}</Text>
+              </div>
+              <div className='flex-between' >
+                <Text> Secondary:</Text>
+                <Text> ${budget[1].amount}</Text>
+              </div>
             </div>
           </div>
-        </div>
         </Card>
-        <Card>
-        <Doughnut data={data} />
+        <Card style={{ width: '150px', flexDirection: 'column' }} hoverable='true'>
+          <Space direction='vertical' style={{ marginTop: '10px', fontSize: '10px' }}>
+          <Progress width='98px' type="circle" percent={75} format={amount => `$${budget[0].amount}`} />
+            <Text> Monthly Limit: 20000  </Text>
+            <Text> Remaining:  15000 </Text>
+          </Space>
         </Card>
       </Space>
     </>
